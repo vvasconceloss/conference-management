d4866e1de7c299342ad4e868b4fc8a84e3deb3e3
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-02-24 13:08:59 +0000

    feat: add header 'component' to login.php page

--- public/pages/login.php
+++ public/pages/login.php
@@ -3,8 +3,28 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/global.css">
     <title>Inovatech | Iniciar Sessão</title>
   </head>
   <body>
+  <header class="header">
+    <div class="header-logo">
+      <img src="../images/inovatech_logo.png" alt="Inovatech Logo" class="logo-image">
+    </div>
+    <nav class="header-nav">
+      <div class="header-nav-links">
+        <a href="../index.php" class="nav-link">Início</a>
+        <a href="#" class="nav-link">Conferências</a>
+      </div>
+      <div class="header-nav-buttons">
+        <a href="./login.php">
+          <button>Iniciar Sessão</button>
+        </a>
+        <a href="./register.php">
+          <button>Criar Conta</button>
+        </a>
+      </div>
+    </nav>  
+  </header>
   </body>
 </html>
