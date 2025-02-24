8744d3deac7b2f169700e19eef3844f972b02891
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-02-24 11:10:14 +0000

    feat: create a initial page (index.php) for website

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/global.css">
+  <link rel="stylesheet" href="./styles/pages/index.css">
+  <title>Inovatech | Gestão de Conferências</title>
+</head>
+<body>
+  <header class="header">
+    <div class="header-logo">
+      <img src="" alt="Inovatech Logo" class="logo-image">
+    </div>
+    <nav class="header-nav">
+      <div class="header-nav-links">
+        <a href="#" class="nav-link">Início</a>
+        <a href="#" class="nav-link">Conferências</a>
+      </div>
+      <div class="header-nav-buttons">
+        <button>Login</button>
+        <button>Registo</button>
+      </div>
+    </nav>
+  </header>
+  <main class="main">
+
+  </main>
+  <footer class="footer">
+
+  </footer>
+</body>
+</html>
