a7ef23d0616c172139f46388a9a225339e0580cc
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-02-24 12:21:11 +0000

    feat: add imagem to index.php and change buttons text

--- public/index.php
+++ public/index.php
@@ -4,13 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./styles/global.css">
-  <link rel="stylesheet" href="./styles/pages/index.css">
+  <link rel="stylesheet" href="./styles/css/index.css">
   <title>Inovatech | Gestão de Conferências</title>
 </head>
 <body>
   <header class="header">
     <div class="header-logo">
-      <img src="" alt="Inovatech Logo" class="logo-image">
+      <img src="./images/inovatech_logo.png" alt="Inovatech Logo" class="logo-image">
     </div>
     <nav class="header-nav">
       <div class="header-nav-links">
@@ -18,10 +18,14 @@
         <a href="#" class="nav-link">Conferências</a>
       </div>
       <div class="header-nav-buttons">
-        <button>Login</button>
-        <button>Registo</button>
+        <a href="./pages/login.php">
+          <button>Iniciar Sessão</button>
+        </a>
+        <a href="./pages/register.php">
+          <button>Criar Conta</button>
+        </a>
       </div>
-    </nav>
+    </nav>  
   </header>
   <main class="main">
 
