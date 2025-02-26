51af5557eaa6e2a82e200a944e4b9c11c03acecc
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-02-26 17:19:48 +0000

    feat: create a file for conferences page (.php)

---
+++ public/pages/protected/conferencias.php
@@ -0,0 +1,17 @@
+<?php
+  session_start();
+  
+  include("../../../config/databaseConnection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inovatech | Conferencias</title>
+</head>
+<body>
+  <h1>Oi</h1>
+</body>
+</html>
