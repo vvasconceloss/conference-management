ecafb06c1d0adab05e6b2b49cee18bf6b88879a1
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-10 12:27:44 +0000

    feat: create the header component on the conference page

--- public/pages/protected/conferencias.php
+++ public/pages/protected/conferencias.php
@@ -9,9 +9,28 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../styles/global.css">
   <title>Inovatech | Conferencias</title>
 </head>
 <body>
-  <h1>Oi</h1>
+  <header class="header">
+      <div class="header-logo">
+        <img src="../../images/inovatech_logo.png" alt="Inovatech Logo" class="logo-image">
+      </div>
+      <nav class="header-nav">
+        <div class="header-nav-links">
+          <a href="../../index.html" class="nav-link">Início</a>
+          <a href="./conferencias.php" class="nav-link">Conferências</a>
+        </div>
+        <div class="header-nav-buttons">
+          <a href="../login.html">
+            <button id="signin">Iniciar Sessão</button>
+          </a>
+          <a href="../register.html">
+            <button id="signup">Criar Conta</button>
+          </a>
+        </div>
+      </nav>  
+    </header>
 </body>
 </html>
